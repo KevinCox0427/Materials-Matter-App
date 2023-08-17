c7919bb4a3a7a69a6ef9162ae0a5e0499b6fda74
Kevin Cox <120033387+KevinCox0427@users.noreply.github.com>
2023-08-17 10:31:30 -0400

    Delete stopMaterialsMatter.bash

--- stopMaterialsMatter.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ps -ef | egrep node | egrep -v grep
-sleep 1
-ps -ef | egrep node | egrep -v grep | awk '{print "kill -9 "$2}'
-ps -ef | egrep node | egrep -v grep | awk '{print "kill -9 "$2}' |sh
-sleep 5
-ps -ef | egrep node | egrep -v grep
